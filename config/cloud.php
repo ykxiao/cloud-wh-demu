a25e672bcee9366476b765110c317c8274cffe5f
ykxiao <yk_9001@hotmail.com>
2020-06-20 21:26:31 +0800

    优化配置文件

--- config/cloud.php
+++ config/cloud.php
@@ -6,30 +6,15 @@
  * Description: 默认配置
  */
 
+/**
+ * 德木云仓服务接口默认配置
+ */
 return [
-    // 德木云仓服务接口默认配置
     'default' => [
-        // 是否要开服务
-        'enabled' => env('CLOUD_WH_ENABLED',true),
-        // 服务端的access_token
-        'token' => env('CLOUD_WH_TOKEN',''),
-        // 服务接口请求的超时时间
-        'timeout' => env('CLOUD_WH_TIME_OUT',2.0),
-        // 是否开启ss认证
-        'ssl_verify' => env('CLOUD_WH_SSL_VERIFY',true),
-        // 开启安全配置
-        'secret' => env('CLOUD_WH_SECRET',true),
-    ],
-
-    'other' => [
-        'enabled' => env('OTHER_DING_ENABLED',true),
-
-        'token' => env('OTHER_DING_TOKEN',''),
-
-        'timeout' => env('OTHER_DING_TIME_OUT',2.0),
-
-        'ssl_verify' => env('CLOUD_WH_SSL_VERIFY',true),
-
-        'secret' => env('CLOUD_WH_SECRET',true),
+        'enabled' => env('CLOUD_WH_ENABLED',true),      #是否要开服务
+        'token' => env('CLOUD_WH_TOKEN',''),            #服务端的access_token
+        'timeout' => env('CLOUD_WH_TIME_OUT',2.0),      #服务接口请求的超时时间
+        'ssl_verify' => env('CLOUD_WH_SSL_VERIFY',true),#是否开启ss认证
+        'secret' => env('CLOUD_WH_SECRET',true),        #开启安全配置
     ]
 ];
